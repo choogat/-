3aca8f9494081a507b7ec8adcfb39fbcce3f4f74
choogat <choogat@gmail.com>
2026-04-17 01:55:10 +0700

    EventExpenses: default filter ดูย้อนหลัง เป็นทั้งหมด

--- web/src/pages/EventExpenses.tsx
+++ web/src/pages/EventExpenses.tsx
@@ -1,5 +1,5 @@
 import LedgerPage from "../components/LedgerPage";
 
 export default function EventExpenses() {
-  return <LedgerPage title="ค่าจัดงานอีเวนต์" ledgerType="EVENT" showPartyFilter />;
+  return <LedgerPage title="ค่าจัดงานอีเวนต์" ledgerType="EVENT" showPartyFilter defaultAllMonths />;
 }
